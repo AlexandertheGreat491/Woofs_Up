8a765f9e1d3411d86da343dfd686999581f85159
agvan <agvandyke25@gmail.com>
2022-09-24 17:29:03 -0500

    updated Header component and added it to the App.js

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="container">
+      <h1>Woof's Up!</h1>
+    </header>
+  );
+}
+
+// exports the Header component so that it can be used by the app
+export default Header;
