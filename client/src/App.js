d66d667bc9a53a1e60f5d61e53ca4a8c8d2e19ae
agvan <agvandyke25@gmail.com>
2022-09-24 17:50:56 -0500

    added the LoginForm, SignUpForm, Nav, Header, and Footer components & installed react-icons

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,27 @@
 //import logo from './logo.svg';
-import Header from'./components/Header';
-import './App.css';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import "./App.css";
 
 function App() {
   return (
-    
-      <header className="App-header">
+    <Router>
+    <div>
+      <div>
         <Header></Header>
-        
-      </header>
-    
+        <Nav></Nav>
+        <Routes>
+          
+        </Routes>
+      </div>
+      <div>
+        <Footer/>
+      </div>
+    </div>
+    </Router>
   );
 }
 
---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { FaGithub } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+      <a href="https://github.com/AlexandertheGreat491">
+        <FaGithub />
+      </a>
+      <span>&copy; 2022 Alexander Van Dyke</span>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
