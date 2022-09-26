cc751d583f5e89abd604a6b5d9d4a5aae8be0f20
agvan <agvandyke25@gmail.com>
2022-09-26 13:56:38 -0500

    updated the form and created the helpers

--- client/src/components/Travel/index.js
+++ client/src/components/Travel/index.js
@@ -1,38 +1,86 @@
-import React from "react";
-// imports googleFormsToJson
-import {
-  GoogleFormProvider,
-  googleFormsToJson,
-} from "react-google-forms-hooks";
-// imports the Google Forms json
-import form from "./form.json";
+import React, { useState } from "react";
+import { validateEmail } from "../../utils/helpers";
 
-// Travel form
-const result = await googleFormsToJson();
-
-
-
+// Travel function to get return statement
 const Travel = () => {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [errorMessage, setErrorMessage] = useState("");
+  const { name, email, message } = formState;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!errorMessage) {
+      setFormState({ [e.target.name]: e.target.value });
+      console.log("Form", formState);
+    }
+  };
+  const handleChange = (e) => {
+    if (e.target.name === "email") {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage("Your email is invalid!");
+      } else {
+        setErrorMessage("");
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage("");
+      }
+    }
+  };
   return (
-    <>
-      <div>
-        <div className="card container">
-          <h2>Shipping</h2>
+    <section>
+      <h1>Contact Me</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            name="name"
+            defaultValue={name}
+            onBlur={handleChange}
+          />
         </div>
-        <div className="card container">
-          <h2>Air</h2>
+        <div>
+          <label htmlFor="email">Email address:</label>
+          <input
+            type="email"
+            name="email"
+            defaultValue={email}
+            onBlur={handleSubmit}
+          />
         </div>
-        <div className="card container">
-          <h2>Sea</h2>
+        <div>
+          <label htmlFor="travel">Travel:</label>
+          <p>Please select the answers that best meet your travel needs.</p>
+          <br></br>
+          <p>Will you be traveling domestically or internationally?</p>
+          <input type="radio" name="domestic" value="Domestically"></input>
+          <label for="domestic">Domestically</label>
+          <input type="radio" name="international" value="Internationally"></input>
+          <label for="international">Internationally</label>
+          <br></br>
+          <p>Will you be travelling with your dog?</p>
+          <input type="radio" name="yes" value="Yes"></input>
+          <label for="yes">Yes</label>
+          <input type="radio" name="no" value="No"></input>
+          <label for="no">No</label>
+          <br></br>
+          
         </div>
-        <div className="card container">
-          <h2>Land</h2>
-        </div>
-        <div className="card container">
-          <h2>Boarding</h2>
-        </div>
-      </div>
-    </>
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+        <button type="submit">Submit</button>
+      </form>
+    </section>
   );
 };
 
