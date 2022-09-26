cec56ac33aebff9e3e6944498b273b22af3db592
agvan <agvandyke25@gmail.com>
2022-09-26 13:01:15 -0500

    removed duplicated README, installed react-icons, and started setting up Google Form in Travel component

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Woofs_Up
+
+[react-google-forms-hooks](https://www.npmjs.com/package/react-google-forms-hooks)
--- client/src/components/Travel/index.js
+++ client/src/components/Travel/index.js
@@ -1,27 +1,39 @@
-import React from 'react';
+import React from "react";
+// imports googleFormsToJson
+import {
+  GoogleFormProvider,
+  googleFormsToJson,
+} from "react-google-forms-hooks";
+// imports the Google Forms json
+import form from "./form.json";
+
+// Travel form
+const result = await googleFormsToJson();
+
+
 
 const Travel = () => {
-    return (
-        <>
-        <div>
-        <div className='card container'>
-            <h2>Shipping</h2>
+  return (
+    <>
+      <div>
+        <div className="card container">
+          <h2>Shipping</h2>
         </div>
-        <div className='card container'>
-             <h2>Air</h2>
+        <div className="card container">
+          <h2>Air</h2>
         </div>
-        <div className='card container'>
-            <h2>Sea</h2>
+        <div className="card container">
+          <h2>Sea</h2>
         </div>
-        <div className='card container'>
-            <h2>Land</h2>
+        <div className="card container">
+          <h2>Land</h2>
         </div>
-        <div className='card container'>
-            <h2>Boarding</h2>
+        <div className="card container">
+          <h2>Boarding</h2>
         </div>
-        </div>
-        </>
-    );
-}
+      </div>
+    </>
+  );
+};
 
 export default Travel;
