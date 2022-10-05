a9a1f8c47926fb20d5dab01100c14cb96d4dab5e
agvan <agvandyke25@gmail.com>
2022-10-05 14:03:39 -0500

    tried something and created the data.json file

--- client/src/components/Travel/index.js
+++ client/src/components/Travel/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useForm } from "react-hook-form";
+//import { useForm } from "react-hook-form";
 import { validateEmail } from "../../utils/helpers";
 import { FaDog } from "react-icons/fa";
 
