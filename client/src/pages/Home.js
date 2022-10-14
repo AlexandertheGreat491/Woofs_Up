8847d4a3b6008bbabe1218d2ba717486c07eef6a
agvan <agvandyke25@gmail.com>
2022-10-14 16:47:56 -0500

    added camera icon

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import dogCanoe from ".//../assets/images/dog_canoe.jpg";
 import dogCar from ".//../assets/images/dog_car.jpg";
+import { AiFillCamera } from "react-icons/ai";
 
 const Home = () => {
   return (
@@ -11,7 +12,7 @@ const Home = () => {
         alt="canoe"
         className="d-inline-flex m-3"
       />
-      <p id="text">Imagine taking your best friend on your adventures</p>
+      <p id="text">Imagine taking your best friend on your adventures<AiFillCamera className="m-1"/></p>
       <img id="car" style={{ width: "15%", height: "15%" }} src={dogCar} alt="car" className="d-inline-flex m-3" />
     </div>
   );
