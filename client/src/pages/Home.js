32eb687b40b9f1053507d50594dd801d61944fcc
agvan <agvandyke25@gmail.com>
2022-10-14 16:56:20 -0500

    added icon

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import dogCanoe from ".//../assets/images/dog_canoe.jpg";
 import dogCar from ".//../assets/images/dog_car.jpg";
 import { AiFillCamera } from "react-icons/ai";
+import { FaTelegramPlane } from "react-icons/fa";
 
 const Home = () => {
   return (
@@ -14,7 +15,7 @@ const Home = () => {
       />
       <p id="text">Imagine taking your best friend on your adventures<AiFillCamera className="m-1"/></p>
       <img id="car" style={{ width: "15%", height: "15%" }} src={dogCar} alt="car" className="d-inline-flex m-3" />
-      <p id="texttwo">We are here to make that possible. Come join us!</p>
+      <p id="texttwo">We are here to make that possible. Come join us!<FaTelegramPlane className="m-1"/></p>
     </div>
   );
 };
