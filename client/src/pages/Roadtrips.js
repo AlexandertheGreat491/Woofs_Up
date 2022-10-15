98bbdd52779a35ead623776a6436db0effa447aa
agvan <agvandyke25@gmail.com>
2022-10-15 14:14:20 -0500

    started the Roadtrips page

---
+++ client/src/pages/Roadtrips.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Roadtrips = () => {
+  return (
+    <div>
+      <h1>Roadtrips with your dog</h1>
+      <p>Plan Plan Plan</p>
+      <p>Start by reading this article</p>
+      <a href="https://be.chewy.com/how-to-travel-with-your-dog-a-complete-guide-to-hitting-the-road-with-your-pup-2/?gclid=CjwKCAjwtKmaBhBMEiwAyINuwJ3pCtt84IeEGNc80_cOnM5oe2b0baHjuKuycUq4GtqwXoPONwx0gRoCGEcQAvD_BwE">
+        How to Travel With Your Dog: A Complete Guide to Hitting the Road With
+        Your Pup
+      </a>
+      <p>Then search our database for everything from dog friendly hotels to dog parks to veterinarians to rest stops</p>
+    </div>
+  );
+};
+
+export default Roadtrips;
