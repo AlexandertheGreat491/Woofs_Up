782f2f71281692d2d083a58013371322161dc7e8
agvan <agvandyke25@gmail.com>
2022-10-16 13:06:47 -0500

    added font-size, font-weight, and color to paragraph

--- client/src/pages/Roadtrips.js
+++ client/src/pages/Roadtrips.js
@@ -7,14 +7,14 @@ const Roadtrips = () => {
     <div className="roadtrip m-1">
       <h1 id="road" className="ms-1">Roadtrips with your dog<FaCarSide className="ms-1"/></h1>
       <p id="roady" className="ms-1">Plan Plan Plan</p>
-      <p className="ms-1">Start by reading this article<FaBookOpen className="ms-1"/></p>
+      <p className="ms-1" id="puppy">Start by reading this article<FaBookOpen className="ms-1"/></p>
       <a href="https://be.chewy.com/how-to-travel-with-your-dog-a-complete-guide-to-hitting-the-road-with-your-pup-2/?gclid=CjwKCAjwtKmaBhBMEiwAyINuwJ3pCtt84IeEGNc80_cOnM5oe2b0baHjuKuycUq4GtqwXoPONwx0gRoCGEcQAvD_BwE"
       className="mb-1 ms-1"
       id="roadlink">
         How to Travel With Your Dog: A Complete Guide to Hitting the Road With
         Your Pup
       </a>
-      <p className="ms-1">Then search our database for everything from dog friendly hotels to dog parks to veterinarians to rest stops</p>
+      <p className="ms-1" id="puppy">Then search our database for everything from dog friendly hotels to dog parks to veterinarians to rest stops.</p>
     </div>
   );
 };
