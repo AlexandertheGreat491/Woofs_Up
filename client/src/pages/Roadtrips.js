2ceef3c4cc339cc69623c65bc1e879b88f7308ea
agvan <agvandyke25@gmail.com>
2022-10-16 12:56:27 -0500

    added car icon

--- client/src/pages/Roadtrips.js
+++ client/src/pages/Roadtrips.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { FaBookOpen } from "react-icons/fa";
+import { FaCarSide } from "react-icons/fa";
 
 const Roadtrips = () => {
   return (
     <div className="roadtrip m-1">
-      <h1 id="road" className="ms-1">Roadtrips with your dog</h1>
+      <h1 id="road" className="ms-1">Roadtrips with your dog<FaCarSide className="ms-1"/></h1>
       <p id="roady" className="ms-1">Plan Plan Plan</p>
       <p className="ms-1">Start by reading this article<FaBookOpen className="ms-1"/></p>
       <a href="https://be.chewy.com/how-to-travel-with-your-dog-a-complete-guide-to-hitting-the-road-with-your-pup-2/?gclid=CjwKCAjwtKmaBhBMEiwAyINuwJ3pCtt84IeEGNc80_cOnM5oe2b0baHjuKuycUq4GtqwXoPONwx0gRoCGEcQAvD_BwE"
