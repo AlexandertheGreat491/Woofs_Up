f8c3d1c3cc88b9219d814b1b72b7351ae6ca6832
agvan <agvandyke25@gmail.com>
2022-10-21 13:34:01 -0500

    added  the name field

--- server/models/Airline.js
+++ server/models/Airline.js
@@ -1,2 +1,12 @@
 const {Schema, model} = require("mongoose");
 const dateFormat = ("../utils/dateFormat");
+
+const airlineSchema = new Schema (
+    {
+        name: {
+            tyep: String, 
+            required: "Please enter the name of the airline",
+            minLength: 1
+        },
+    }
+)
