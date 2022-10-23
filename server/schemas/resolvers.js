4eb8abe69eb47f553b195fe1ba35d44975bbdd4e
agvan <agvandyke25@gmail.com>
2022-10-23 14:39:11 -0500

    added three comments

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,6 @@
+// imports apollo-server-express
 const { AuthenticationError } = require("apollo-server-express");
+// imports all three models
 const { User, Hotel, Airline } = require("../models");
+// imports signToken
 const { signToken } = require("..utils/auth");
