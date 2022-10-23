86a27a8b36bb075d88f5e2fef33d28c1308d821a
agvan <agvandyke25@gmail.com>
2022-10-23 14:12:46 -0500

    added User typeDef

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,11 @@
 const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+type User {
+    _id: ID
+    username: String
+    email: String
+    hotel: [Hotel]
+    airline: [Airline]
+}
+`
