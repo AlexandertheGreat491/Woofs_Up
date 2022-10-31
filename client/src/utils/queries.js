8ff59378e37aee2d4e13cc93df5841ab116cf662
agvan <agvandyke25@gmail.com>
2022-10-31 13:27:40 -0500

    added query for a single hotel

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -15,3 +15,15 @@ export const QUERY_HOTELS = gql`
 `;
 
 // query for a single hotel
+export const QUERY_HOTEL = gql`
+query Query($id: ID:) {
+  hotel(_id: $id) {
+    _id
+    name
+    description
+    createdAt
+    username
+    url
+  }
+}
+`;
