9aaef8ff5dc1130a71fd9335f4c1e0026a42d22c
agvan <agvandyke25@gmail.com>
2023-01-16 12:12:49 -0600

    attempted to add home button & changed margins

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,23 +6,24 @@ function Header() {
   return (
     <header className="col-12 d-inline-flex pb-3">
       <div>
-        
         <Link to="/">
-          <h1 id="navheader" className="d-inline-flex w-100 text-center">Woof's Up!</h1>
+          <h1 id="navheader" className="d-inline-flex w-100 text-center">
+            Woof's Up!
+          </h1>
         </Link>
 
         <div id="nav" className="ms-4 mt-1">
           <nav className="text-center d-inline-flex flex-row">
             <>
-              
-              <Link className="navlinks" to="/travel">
+              <Link className="navlinks ms-5" to="/home">
+                Home
+              </Link>
+              <Link className="navlinks ms-5" to="/travel">
                 Travel
               </Link>
             </>
           </nav>
-          
         </div>
-  
       </div>
     </header>
   );
