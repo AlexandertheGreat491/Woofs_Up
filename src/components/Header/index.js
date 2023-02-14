cce8cf0b5ba97c055c64b82f745b513bce76986e
agvan <agvandyke25@gmail.com>
2023-02-13 18:47:45 -0600

    changed margins

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,46 +4,49 @@ import { Link } from "react-router-dom";
 // imports the logo
 import woofsUpLogo from "..//../assets/images/LOGO.jpg";
 // imports the paw icon
-import {FaPaw} from "react-icons/fa";
+import { FaPaw } from "react-icons/fa";
 
 function Header() {
   return (
     <header className="col-12 d-inline-flex pb-3">
       <div>
         <Link to="/">
-          <h1 id="navheader" className="d-inline-flex w-100 justify-content-center ms-4">
+          <h1
+            id="navheader"
+            className="d-inline-flex w-100 justify-content-center ms-4"
+          >
             Woof's Up!
           </h1>
         </Link>
 
-        <div id="nav" className="ms-5 mt-1">
+        <div id="nav" className="ms-5 mt-3">
           <nav className="text-center d-inline-flex flex-row">
             <>
-              <Link className="navlinks me-3" to="/">
+              <Link className="navlinks me-3 ms-4 mb-2" to="/">
                 Home
               </Link>
-              <FaPaw className="m-2" />
-              <Link className="navlinks me-3" to="/travel">
+              <FaPaw className="mt-3" />
+              <Link className="navlinks me-3 ms-4 mb-2" to="/travel">
                 Travel
               </Link>
-              <FaPaw className="m-2" />
-              <Link className="navlinks me-3" to="/AllTravel">
+              <FaPaw className="mt-3" />
+              <Link className="navlinks me-3 ms-4 mb-2" to="/AllTravel">
                 Plains, Trains, and Automobiles
               </Link>
-              <FaPaw className="m-2" />
-              <Link className="navlinks" to="/Boarding">
+              <FaPaw className="mt-3" />
+              <Link className="navlinks ms-4 mb-2" to="/Boarding">
                 Boarding
               </Link>
             </>
           </nav>
           {/*woofs up logo image */}
-<img
-        src={woofsUpLogo}
-        style={{ width: "15%", height: "15%" }}
-        alt="logo"
-        className="img-fluid rounded mx-auto d-block"
-        id="woof"
-      />
+          <img
+            src={woofsUpLogo}
+            style={{ width: "15%", height: "15%" }}
+            alt="logo"
+            className="img-fluid rounded mx-auto d-block"
+            id="woof"
+          />
         </div>
       </div>
     </header>
