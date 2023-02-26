9722d0966aff4d0952a663ad231b21a56a347723
agvan <agvandyke25@gmail.com>
2023-02-26 14:04:53 -0600

    cleaned up code

--- src/pages/AllTravel.js
+++ src/pages/AllTravel.js
@@ -16,10 +16,7 @@ import { FaHouseUser } from "react-icons/fa";
 import { FaLocationArrow } from "react-icons/fa";
 
 const AllTravel = () => {
-
   return (
-    
-
     <div className="roadtrip ms-1 me-4" style={{ width: "45%", height: "15%" }}>
       <h1 id="road" className="ms-1">
         Roadtrips with your dog
@@ -32,23 +29,23 @@ const AllTravel = () => {
         Start by reading this article
         <FaBookOpen className="ms-1" />
       </p>
-      
+
       <a
         href="https://be.chewy.com/how-to-travel-with-your-dog-a-complete-guide-to-hitting-the-road-with-your-pup-2/?gclid=CjwKCAjwtKmaBhBMEiwAyINuwJ3pCtt84IeEGNc80_cOnM5oe2b0baHjuKuycUq4GtqwXoPONwx0gRoCGEcQAvD_BwE"
         className="mb-1"
         id="roadlink"
       >
         <img
-      src={guide}
-      style={{ width: "18%", height: "18%" }}
-      alt="thumbnail"
-      id="article"
-      className="me-1"
-      ></img>
+          src={guide}
+          style={{ width: "18%", height: "18%" }}
+          alt="thumbnail"
+          id="article"
+          className="me-1"
+        ></img>
         How to Travel With Your Dog: A Complete Guide to Hitting the Road With
         Your Pup
       </a>
-      
+
       {/* <div className="m-1">
         <h2 id="air">Fly higher with your dog<FaPlaneDeparture className="ms-1"/></h2>
         <p id="airtravel">Check out the helpful links and then search the database for further information!</p>
@@ -57,47 +54,39 @@ const AllTravel = () => {
         <a id="airlink" href="https://www.bringfido.com/travel/us_regulations/">US Pet Air Travel Regulations</a>
         <a id="airlink" href="https://www.state.gov/pets-and-international-travel/" >Pets and International Travel</a>
       </div> */}
-    {/* </div> */}
+      {/* </div> */}
 
-    <div id="doghotel" className="container card m-1">
-  <h2 id="board">
-    What you need know about boarding your dog
-    <FaHouseUser className="ms-1" />
-  </h2>
-  <a
-    id="boardinglinks"
-    href="https://be.chewy.com/everything-you-need-to-know-about-pet-boarding/?gclid=Cj0KCQjwy5maBhDdARIsAMxrkw1xTCLzJeWX-StxKYy2i39BxGjJqJkFdb5LvTOxoyK_VZNsKomNLSwaAuUGEALw_wcB"
-  >
-    Everything you need to know about pet boarding
-  </a>
+      <div id="doghotel" className="container card m-1">
+        <h2 id="board">
+          What you need know about boarding your dog
+          <FaHouseUser className="ms-1" />
+        </h2>
+        <a
+          id="boardinglinks"
+          href="https://be.chewy.com/everything-you-need-to-know-about-pet-boarding/?gclid=Cj0KCQjwy5maBhDdARIsAMxrkw1xTCLzJeWX-StxKYy2i39BxGjJqJkFdb5LvTOxoyK_VZNsKomNLSwaAuUGEALw_wcB"
+        >
+          Everything you need to know about pet boarding
+        </a>
 
-  <h2 id="board">
-    Search for the best accomodations by state
-    <FaLocationArrow className="m-1" />
-  </h2>
-  <img
-    style={{ width: "15%", height: "15%" }}
-    src={dogSuitcase}
-    alt="dog"
-    className="m-1"
-  />
-  <a
-    id="boardinglinks"
-    href="https://www.bringfido.com/resource/doggie_daycare_boarding/region/united_states/"
-  >
-    Boarding by state
-  </a>
-</div>
-    
-</div>
-
-    
-
-
-
-
+        <h2 id="board">
+          Search for the best accomodations by state
+          <FaLocationArrow className="m-1" />
+        </h2>
+        <img
+          style={{ width: "15%", height: "15%" }}
+          src={dogSuitcase}
+          alt="dog"
+          className="m-1"
+        />
+        <a
+          id="boardinglinks"
+          href="https://www.bringfido.com/resource/doggie_daycare_boarding/region/united_states/"
+        >
+          Boarding by state
+        </a>
+      </div>
+    </div>
   );
 };
-
 
 export default AllTravel;
